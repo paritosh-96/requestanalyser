e97b7f303fd2998e97263ed644b277ebe815d27b
paritosh_18 <milendu.paritosh@customerxps.com>
2020-12-20 19:44:59 +0530

    Initial commit

---
+++ src/main/java/com/test/requestanalyser/RequestanalyserApplication.java
@@ -0,0 +1,13 @@
+package com.test.requestanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestanalyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestanalyserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/requestanalyser/RequestanalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.requestanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestanalyserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
